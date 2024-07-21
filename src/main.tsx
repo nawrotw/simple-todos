971d8974511855dffd494cff993e12c1ab09cdbf
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-07-21 07:23:24 +0200

    Speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import { CssBaseline } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { todosApiMock } from "./api/todos/mocks/todosApiMock.ts";
 import { setupWorker } from "msw/browser";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 const queryClient = new QueryClient();
@@ -27,5 +28,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <App/>
     </QueryClientProvider>
+    <SpeedInsights/>
   </React.StrictMode>,
 )
